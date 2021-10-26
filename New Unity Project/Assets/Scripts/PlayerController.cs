614b6393aeea792108fd4a4489a48de77d669d19
Anika Boettcher <10945351@my.uvu.edu>
2021-10-26 16:33:19 -0600

    Added Banana prefab

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
 
     public float xRange = 10.0f;
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
